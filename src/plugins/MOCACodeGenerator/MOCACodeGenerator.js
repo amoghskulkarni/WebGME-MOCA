ac29cc1e3ae388f8fefd4b819fe91047441f335e
amoghkulkarni <amogh.s.kulkarni@vanderbilt.edu>
2016-02-03 20:35:41 -0600

    Seed (json file of the project) for MOCA_2 added, components.py generates OK

--- src/plugins/MOCACodeGenerator/MOCACodeGenerator.js
+++ src/plugins/MOCACodeGenerator/MOCACodeGenerator.js
@@ -133,7 +133,7 @@ define([
                 problemPromises = [];
 
             for (var i = 0; i < children.length; i += 1) {
-                if (self.core.getAttribute(self.getMetaType(children[i]), 'name') == 'Component')
+                if (self.core.getAttribute(self.getMetaType(children[i]), 'name') == 'Component') 
                     componentPromises.push(self.getComponentData(children[i]));
                 // else if (self.core.getAttribute(self.getMetaType(children[i]), 'name') == 'Group')
                 //     groupPromises.push(self.getGroupData(children[i]));
