e7b5ae1f24ea3104c6555cf8cd576d851f802399
amoghkulkarni <amogh.s.kulkarni@vanderbilt.edu>
2016-02-15 20:02:05 -0600

    Text editor for adding business logic in Components

--- src/decorators/CodeEditorDecorator/DiagramDesigner/CodeEditorDecorator.DiagramDesignerWidget.js
+++ src/decorators/CodeEditorDecorator/DiagramDesigner/CodeEditorDecorator.DiagramDesignerWidget.js
@@ -1,32 +1,44 @@
 /*globals define, _, $*/
 /*jshint browser: true, camelcase: false*/
 
-/**
- * @author rkereskenyi / https://github.com/rkereskenyi
- */
-
 define([
-    // 'js/Constants',
-    // 'js/NodePropertyNames',
-    // 'js/Widgets/DiagramDesigner/DiagramDesignerWidget.DecoratorBase',
-    // 'text!./CodeEditorDecorator.DiagramDesignerWidget.html',
-    // 'css!./CodeEditorDecorator.DiagramDesignerWidget.css'
-// ], function (CONSTANTS, nodePropertyNames, ModelDecoratorDiagramDesignerWidget, CodeEditorDecoratorTemplate) {
-
+    'js/RegistryKeys',
+    'js/Constants',
+    '../Libs/EpicEditor/js/epiceditor',
+    './DocumentEditorDialog',
     'decorators/ModelDecorator/DiagramDesigner/ModelDecorator.DiagramDesignerWidget'
-  ], function (ModelDecoratorDiagramDesignerWidget) {
+  ], function (
+    REGISTRY_KEYS,
+    CONSTANTS,
+    marked,
+    DocumentEditorDialog,
+    ModelDecoratorDiagramDesignerWidget) {
 
     'use strict';
 
     var CodeEditorDecorator,
-        DECORATOR_ID = 'CodeEditorDecorator';
+        DECORATOR_ID = 'CodeEditorDecorator',
+        TEXT_META_EDIT_BTN_BASE = $('<i class="glyphicon glyphicon-edit text-meta"/>');
 
     CodeEditorDecorator = function (options) {
         var opts = _.extend({}, options);
 
         ModelDecoratorDiagramDesignerWidget.apply(this, [opts]);
 
-        // this.name = '';
+        this.editorDialog = new DocumentEditorDialog();
+
+        this._skinParts = {};
+
+        // Use default marked options
+        marked.setOptions({
+            gfm: true,
+            tables: true,
+            breaks: false,
+            pedantic: false,
+            sanitize: true,
+            smartLists: true,
+            smartypants: false
+        });
 
         this.logger.debug('CodeEditorDecorator ctor');
     };
@@ -37,155 +49,70 @@ define([
 
     /*********************** OVERRIDE ModelDecoratorDiagramDesignerWidget MEMBERS **************************/
 
-    // CodeEditorDecorator.prototype.$DOMBase = $(CodeEditorDecoratorTemplate);
-
     CodeEditorDecorator.prototype.on_addTo = function () {
-        // var self = this;
-        //
-        // this._renderName();
-        //
-        // // set title editable on double-click
-        // this.skinParts.$name.on('dblclick.editOnDblClick', null, function (event) {
-        //     if (self.hostDesignerItem.canvas.getIsReadOnlyMode() !== true) {
-        //         $(this).editInPlace({
-        //             class: '',
-        //             onChange: function (oldValue, newValue) {
-        //                 self._onNodeTitleChanged(oldValue, newValue);
-        //             }
-        //         });
-        //     }
-        //     event.stopPropagation();
-        //     event.preventDefault();
-        // });
-        //
-
+        var self = this,
+            client = this._control._client,
+            nodeObj = client.getNode(this._metaInfo[CONSTANTS.GME_ID]);
 
         //let the parent decorator class do its job first
         ModelDecoratorDiagramDesignerWidget.prototype.on_addTo.apply(this, arguments);
-        //
-        this._btn = $('<button>Test</button>');
-        this._btn.on('click', function () {
-            alert('Hello!');
+
+        // Initialize dialog with EpicEditor.
+        this._initDialog();
+
+        //render text-editor based META editing UI piece
+        this._skinParts.$EditorBtn = TEXT_META_EDIT_BTN_BASE.clone();
+        this.$el.append('<br>');
+        this.$el.append(this._skinParts.$EditorBtn);
+
+        // onClick listener for the button
+        this._skinParts.$EditorBtn.on('click', function () {
+            if (self.hostDesignerItem.canvas.getIsReadOnlyMode() !== true &&
+                nodeObj.getAttribute('OutputFunction') !== undefined) {
+                self.editorDialog.show();
+            }
+            event.stopPropagation();
+            event.preventDefault();
         });
-        this.$el.append(this._btn);
+
+    };
+
+    CodeEditorDecorator.prototype._initDialog = function () {
+        var self = this;
+        var client = this._control._client;
+        var nodeObj = client.getNode(this._metaInfo[CONSTANTS.GME_ID]);
+        var OutputFunction = nodeObj.getAttribute('OutputFunction') || 'Click to enter Input(s) to Output(s) relation.';
+
+        // Initialize with OutputFunction attribute and save callback function
+        this.editorDialog.initialize(OutputFunction,
+            function (text) {
+                try {
+                    client.setAttributes(self._metaInfo[CONSTANTS.GME_ID], 'OutputFunction', text);
+                } catch (e) {
+                    self.logger.error('Saving META failed... Either not JSON object or something else went wrong...');
+                }
+            }
+        );
     };
 
     CodeEditorDecorator.prototype.destroy = function () {
-        this._btn.off('click');
+        this._skinParts.$EditorBtn.off('click');
         ModelDecoratorDiagramDesignerWidget.prototype.destroy.apply(this, arguments);
     };
 
-    // CodeEditorDecorator.prototype._renderName = function () {
-    //     var client = this._control._client,
-    //         nodeObj = client.getNode(this._metaInfo[CONSTANTS.GME_ID]);
-    //
-    //     //render GME-ID in the DOM, for debugging
-    //     this.$el.attr({'data-id': this._metaInfo[CONSTANTS.GME_ID]});
-    //
-    //     if (nodeObj) {
-    //         this.name = nodeObj.getAttribute(nodePropertyNames.Attributes.name) || '';
-    //     }
-    //
-    //     //find name placeholder
-    //     this.skinParts.$name = this.$el.find('.name');
-    //     this.skinParts.$name.text(this.name);
-    // };
-
     CodeEditorDecorator.prototype.update = function () {
-        // var client = this._control._client,
-        //     nodeObj = client.getNode(this._metaInfo[CONSTANTS.GME_ID]),
-        //     newName = '';
-        //
-        // if (nodeObj) {
-        //     newName = nodeObj.getAttribute(nodePropertyNames.Attributes.name) || '';
-        //
-        //     if (this.name !== newName) {
-        //         this.name = newName;
-        //         this.skinParts.$name.text(this.name);
-        //     }
-        // }
+        var client = this._control._client,
+            nodeObj = client.getNode(this._metaInfo[CONSTANTS.GME_ID]),
+            newDoc = '';
 
         ModelDecoratorDiagramDesignerWidget.prototype.update.apply(this, arguments);
+
+        if (nodeObj) {
+            newDoc = nodeObj.getAttribute('OutputFunction') || '';
+            // Update text in the editor when attribute "OutputFunction" changes
+            this.editorDialog.updateText(newDoc);
+        }
     };
-
-    // CodeEditorDecorator.prototype.getConnectionAreas = function (id /*, isEnd, connectionMetaInfo*/) {
-    //     var result = [],
-    //         edge = 10,
-    //         LEN = 20;
-    //
-    //     //by default return the bounding box edge's midpoints
-    //
-    //     if (id === undefined || id === this.hostDesignerItem.id) {
-    //         //NORTH
-    //         result.push({
-    //             id: '0',
-    //             x1: edge,
-    //             y1: 0,
-    //             x2: this.hostDesignerItem.getWidth() - edge,
-    //             y2: 0,
-    //             angle1: 270,
-    //             angle2: 270,
-    //             len: LEN
-    //         });
-    //
-    //         //EAST
-    //         result.push({
-    //             id: '1',
-    //             x1: this.hostDesignerItem.getWidth(),
-    //             y1: edge,
-    //             x2: this.hostDesignerItem.getWidth(),
-    //             y2: this.hostDesignerItem.getHeight() - edge,
-    //             angle1: 0,
-    //             angle2: 0,
-    //             len: LEN
-    //         });
-    //
-    //         //SOUTH
-    //         result.push({
-    //             id: '2',
-    //             x1: edge,
-    //             y1: this.hostDesignerItem.getHeight(),
-    //             x2: this.hostDesignerItem.getWidth() - edge,
-    //             y2: this.hostDesignerItem.getHeight(),
-    //             angle1: 90,
-    //             angle2: 90,
-    //             len: LEN
-    //         });
-    //
-    //         //WEST
-    //         result.push({
-    //             id: '3',
-    //             x1: 0,
-    //             y1: edge,
-    //             x2: 0,
-    //             y2: this.hostDesignerItem.getHeight() - edge,
-    //             angle1: 180,
-    //             angle2: 180,
-    //             len: LEN
-    //         });
-    //     }
-    //
-    //     return result;
-    // };
-
-    /**************** EDIT NODE TITLE ************************/
-
-    // CodeEditorDecorator.prototype._onNodeTitleChanged = function (oldValue, newValue) {
-    //     var client = this._control._client;
-    //
-    //     client.setAttributes(this._metaInfo[CONSTANTS.GME_ID], nodePropertyNames.Attributes.name, newValue);
-    // };
-
-    /**************** END OF - EDIT NODE TITLE ************************/
-
-    // CodeEditorDecorator.prototype.doSearch = function (searchDesc) {
-    //     var searchText = searchDesc.toString();
-    //     if (this.name && this.name.toLowerCase().indexOf(searchText.toLowerCase()) !== -1) {
-    //         return true;
-    //     }
-    //
-    //     return false;
-    // };
 
     return CodeEditorDecorator;
 });
