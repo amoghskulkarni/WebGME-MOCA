18186aeda5c745091ac2ee05b2b3a6ed884a1414
amoghkulkarni <amogh.s.kulkarni@vanderbilt.edu>
2016-07-05 17:58:35 -0500

    Work resumed: seed updated and minor feature addition

--- config/config.default.js
+++ config/config.default.js
@@ -7,6 +7,14 @@ var config = require('./config.webgme'),
 // config.server.port = 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
 
+//// Plugins
+//config.plugin.allowServerExecution = true;
+//config.plugin.allowBrowserExecution = true;
+//
+//// Seeds
+//config.seedProjects.enable = true;
+//config.seedProjects.basePaths = ["./src/seeds"];
+
 config.visualization.svgDirs = ['./Icons/png'];
 
 validateConfig(config);
