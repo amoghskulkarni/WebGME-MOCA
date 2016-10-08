6fe4e9b1882e7b71028f62f992df5f42a4d4001f
amoghkulkarni <amogh.s.kulkarni@vanderbilt.edu>
2016-10-07 20:50:59 -0500

    Seeds updated and organized clearly. Small changes in app.js and default configs to make it work on server.

--- config/config.default.js
+++ config/config.default.js
@@ -6,6 +6,8 @@ var config = require('./config.webgme'),
 // Add/overwrite any additional settings here
 // config.server.port = 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
+config.server.port = 8888;
+//config.mongo.uri = 'mongodb://127.0.0.1:27017/WebGME-MOCA-2';
 
 // Plugins
 config.plugin.allowServerExecution = true;
@@ -13,7 +15,8 @@ config.plugin.allowBrowserExecution = true;
 
 // Seeds
 config.seedProjects.enable = true;
-config.seedProjects.basePaths = ["./src/seeds"];
+//config.seedProjects.defaultProject = 'MOCA';
+config.seedProjects.basePaths.push("./src/seeds");
 
 config.visualization.svgDirs = ['./Icons/png'];
 
