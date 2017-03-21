6da8feb1d8ed8ec49d4b49e5ae4b45d0a57c8d44
amoghkulkarni <amogh.s.kulkarni@vanderbilt.edu>
2017-03-21 18:39:01 -0500

    Name changed again to make, mongodb uri fixed

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -25,7 +25,7 @@ config.requirejsPaths = {
   'widgets': './src/visualizers/widgets'
 };
 
-
-config.mongo.uri = 'mongodb://127.0.0.1:27017/WebGME_MOCA';
+// TODO: Fix mongo uri as per the package name which is 'WebGME_MOCA'
+config.mongo.uri = 'mongodb://127.0.0.1:27017/moca_2';
 validateConfig(config);
 module.exports = config;
